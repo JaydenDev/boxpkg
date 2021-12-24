c5cf508180e6254dce3e1514f419ec1f62f5b591
JaydenDev <willsjayden2@gmail.com>
2021-12-24 14:52:01 -0600

    COMPLETELY automatic

--- run/catalyst-install.sh
+++ run/catalyst-install.sh
@@ -6,3 +6,19 @@ npm install
 echo "Building for your platform"
 npm run tailwind
 npm run make
+cd out
+cd make
+cd x64
+which dpkg || distrib=RPM
+which rpm || distrib=DEB
+if [[ $distrib == "DEB" ]]
+then
+	cd deb
+	sudo dpkg -i *.deb
+fi
+
+if [[ $distrib == "RPM" ]]
+	cd deb
+	sudo rpm -i *.rpm
+fi
+echo Installed succesfuly 
